bb56cd82629a1cac945e86aba374767f80eb9e74
alextejido <elteji98@gmail.com>
2016-10-18 08:44:48 +0200

    Update Boletin_3.java

--- src/boletin_3/Boletin_3.java
+++ src/boletin_3/Boletin_3.java
@@ -9,7 +9,7 @@ public class Boletin_3 {
        Consumo con1 = new Consumo();
        con1.setLitros(50f);
        con1.setPGas(1.57f);
-       Consumo con2 = new Consumo(247f,20f,1.8f,10f);
+       Consumo con2 = new Consumo(200f,20f,1.8f,10f);
        System.out.println("El consumo medio del consumo 2 es de " + con2.consumoMedio());
         con2.setLitros(70f);
         System.out.println("La velocidad media del consumo 2 es de " + con2.getVMedia());
